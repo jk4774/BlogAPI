aef6b2bf2b672daa834f67e3590ef6ef3d495ab9
jk4774 <kjurczak10@gmail.com>
2019-01-30 21:28:06 +0100

    middlewares

--- Blog.API/Middlewares/TokenProviderMiddleware.cs
+++ Blog.API/Middlewares/TokenProviderMiddleware.cs
@@ -1,62 +1,89 @@
-﻿//using Blog.API.Providers;
-//using Microsoft.AspNetCore.Http;
-//using System;
-//using System.IdentityModel.Tokens.Jwt;
-//using System.Security.Claims;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
 
-//namespace Blog.API.Middlewares
-//{
-//    public class TokenProviderMiddleware
-//    {
-//        private readonly RequestDelegate _requestDelegate;
-//        private readonly TokenProviderOptions _tokenProviderOptions;
+namespace WebApplication2
+{
+    public class TokenProviderMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private readonly TokenProviderOptions _tokenProviderOptions;
 
-//        public TokenProviderMiddleware(RequestDelegate requestDelegate, TokenProviderOptions tokenProviderOptions)
-//        {
-//            _requestDelegate = requestDelegate;
-//            _tokenProviderOptions = tokenProviderOptions;
-//        }
+        public TokenProviderMiddleware(RequestDelegate requestDelegate, IOptions<TokenProviderOptions> tokenProviderOptions)
+        {
+            _requestDelegate = requestDelegate;
+            _tokenProviderOptions = tokenProviderOptions.Value;
+        }
 
-//        public Task Invoke(HttpContext httpContext)
-//        {
-//            if (!httpContext.Request.Method.Equals("POST") || !httpContext.Request.HasFormContentType)
-//            {
-//                httpContext.Response.StatusCode = 400;
-//                return httpContext.Response.WriteAsync("Bad req");
-//            }
-//            return new Task(() => { });
-//        }
+        public Task Invoke (HttpContext httpContext)
+        {
+            if (!httpContext.Request.Path.Equals(_tokenProviderOptions.Path, StringComparison.Ordinal))
+            {
+                return _requestDelegate(httpContext);
+            }
 
-//        private async Task GenerateToken(HttpContext httpContext)
-//        {
-//            var name = httpContext.Request.Form["username"];
-//            var password = httpContext.Request.Form["password"];
-//            var identify = await GetIdentify(name, password);
+            if (!httpContext.Request.Method.Equals("POST") || !httpContext.Request.HasFormContentType)
+            {
+                httpContext.Response.StatusCode = 400;
+                return httpContext.Response.WriteAsync("Bad request...");
+            }
+            return GenerateToken(httpContext);
+        }
 
-//            if (identify == null)
-//            {
-//                httpContext.Response.StatusCode = 404;
-//                await httpContext.Response.WriteAsync("Invalid name or password");
-//                return;
-//            }
+        private async Task GenerateToken(HttpContext httpContext)
+        {
+            var username = httpContext.Request.Form["username"];
+            var password = httpContext.Request.Form["password"];
 
-//            var claims = new Claim[]
-//            {
-//                new Claim(ClaimTypes.Name, Guid.NewGuid().ToString())
-//            };
-            
-//            //var tokenHandler = new JwtSecurityTokenHandler();
-//            //var tokenDescriptor = new SecurityTokenDescriptor
-//            //{
-//            //    Subject = new ClaimsIdentity(new Claim[] { new Claim(ClaimTypes.Name, user.Id.ToString()) }),
-//            //    Expires = DateTime.Now.AddHours(1),
-//            //    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_settings.SecurityKey)), SecurityAlgorithms.HmacSha256Signature)
-//            //};
-//            //var securityToken = tokenHandler.CreateToken(tokenDescriptor);
-//            //return new User { Id = user.Id, Name = user.Name, Password = "", Email = user.Email, Token = tokenHandler.WriteToken(securityToken) };
+            var identify = await GetIdentity(username, password);
+            if (identify == null)
+            {
+                httpContext.Response.StatusCode = 400;
+                await httpContext.Response.WriteAsync("Invalid username/password");
+                return;
+            }
+
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToUniversalTime().ToString(), ClaimValueTypes.Integer64)
+            };
+
+            var jwt = new JwtSecurityToken
+            (
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.Add(_tokenProviderOptions.Expiration),
+                signingCredentials: _tokenProviderOptions.SigningCredentials
+            );
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
 
 
-//        }
-//    }
-//}
+            httpContext.Response.ContentType = "application/json";
+            await httpContext.Response.WriteAsync
+            (
+                JsonConvert.SerializeObject
+                (
+                    new { access_token = encodedJwt, expires_in = (int)_tokenProviderOptions.Expiration.TotalSeconds }, 
+                    new JsonSerializerSettings { Formatting = Formatting.Indented }
+                )
+            );
+        }
+
+        private Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+            //test
+            if (username == "r" && password == "r")
+            {
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(username, "Token"), new Claim[] { }));
+            }
+            return Task.FromResult<ClaimsIdentity>(null);
+        }
+    }
+}
