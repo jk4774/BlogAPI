9eba422954c547036aa5b93b8d2f4894e9e55b97
jurczakk <kjurczak10@gmail.com>
2020-09-19 00:19:48 +0200

    added a few more test

--- BlogTests/Controllers/ArticleControllerTests.cs
+++ BlogTests/Controllers/ArticleControllerTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Security.Principal;
+using System.Threading.Tasks;
 using BlogContext;
 using BlogData.Entities;
 using BlogData.ViewModels;
@@ -92,14 +93,105 @@ namespace BlogTests.Controllers
             Assert.IsInstanceOf<NotFoundResult>(result);
         }
 
-        // public IActionResult Update(int id)
-        // {
-        //     var article = _blogDbContext.Articles.Find(id);
-        //     if (article == null || !article.UserId.ToString().Equals(User.Identity.Name))
-        //         return NotFound();
-            
-        //     return View(article);
-        // }
+        [Test]
+        public void GET_Update_ArticleUserIdIsDoesNotEqualUserId_ShouldReturnNotFound()
+        {
+            A.CallTo(() => fakeIdentity.Name).Returns("3");
+            context.User = principal;
+            A.CallTo(() => httpContextAccessor.HttpContext).Returns(context);
 
+            articleController = new ArticleController(fakeBlog)
+            {
+                ControllerContext = new ControllerContext { HttpContext = httpContextAccessor.HttpContext }
+            };
+
+            var result = articleController.Update(1) as NotFoundResult;
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public void GET_Update_ArticleExistsAndTheUserIdOk_ShouldReturnView()
+        {
+            var result = articleController.Update(1) as ViewResult;
+
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+
+        [Test]
+        public async Task PUT_Update_ArticleDoesNotExistInDb_ShouldReturnNotFound()
+        {
+            var result = await articleController.Update(3, A.Fake<Article>()) as NotFoundResult;
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task PUT_Update_ArticleUserIdIsDoesNotEqualUserId_ShouldReturnNotFound()
+        {
+            A.CallTo(() => fakeIdentity.Name).Returns("3");
+            context.User = principal;
+            A.CallTo(() => httpContextAccessor.HttpContext).Returns(context);
+
+            articleController = new ArticleController(fakeBlog)
+            {
+                ControllerContext = new ControllerContext { HttpContext = httpContextAccessor.HttpContext }
+            };
+
+            var result = await articleController.Update(1, A.Fake<Article>()) as NotFoundResult;
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task PUT_Update_ArticleUserIdIsDoesNotEqualUserId_ShouldReturnNoContent()
+        {
+            var result = await articleController.Update(1, A.Fake<Article>()) as NoContentResult;
+
+            Assert.IsInstanceOf<NoContentResult>(result);
+        }
+
+        [Test]
+        public async Task DELETE_Delete_ArticleDoesNotExist_ShouldReturnNotFound()
+        {
+            var result = await articleController.Delete(3) as NotFoundResult;
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task DELETE_ArticleUserIdIsNotMatchingIdentityName_ShouldReturnNotFound()
+        {
+            A.CallTo(() => fakeIdentity.Name).Returns("3");
+            context.User = principal;
+            A.CallTo(() => httpContextAccessor.HttpContext).Returns(context);
+
+            articleController = new ArticleController(fakeBlog)
+            {
+                ControllerContext = new ControllerContext { HttpContext = httpContextAccessor.HttpContext }
+            };
+
+            var result = await articleController.Delete(1) as NotFoundResult;
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task DELETE_Delete_ArticleIdIsCorrectShouldRemoveTheArticleWithAllTheComments_ShouldReturnNoContent()
+        {
+            var result = await articleController.Delete(1) as NoContentResult;
+
+            Assert.IsInstanceOf<NoContentResult>(result);
+        }
+            
+//             _blogDbContext.Articles.Remove(article);
+//             var comments = _blogDbContext.Comments.Where(x => x.ArticleId == article.Id).ToList();
+//             if (comments.Count > 0)
+//                 _blogDbContext.Comments.RemoveRange(comments);
+
+//             _blogDbContext.SaveChanges();
+
+//             return NoContent();
+        
     }
 }
